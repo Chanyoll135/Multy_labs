07d654dc37886eee2aa23c174acb2236871fc3fb
Chanyoll135 <158028703+Chanyoll135@users.noreply.github.com>
2024-09-13 12:37:56 +0300

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string[]> lines = new List<string[]>();
+        lines.Add(new string[] { "Line 1", "Line 2", "Line 3" });
+        lines.Add(new string[] { "Line A", "Line B", "Line C" });
+        lines.Add(new string[] { "Line X", "Line Y", "Line Z" });
+        lines.Add(new string[] { "Line !", "Line @", "Line #" });
+
+        Parallel.ForEach(lines, (line) =>
+        {
+            foreach (string message in line)
+            {
+                Console.WriteLine(message);
+                Thread.Sleep(100); // Имитация длительной работы
+            }
+        });
+    }
+}
