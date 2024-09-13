f6c1bdd524e92915d02ba6ba695e68f6961fc6ac
Chanyoll135 <158028703+Chanyoll135@users.noreply.github.com>
2024-09-13 12:38:54 +0300

    Add files via upload

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        CancellationToken token = cancellationTokenSource.Token;
+
+        Task.Factory.StartNew(() => ChildThread(token), token);
+
+        Thread.Sleep(2000); // Ждем две секунды
+
+        cancellationTokenSource.Cancel(); // Отменяем задачу
+
+        Console.ReadKey();
+    }
+
+    static void ChildThread(CancellationToken token)
+    {
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("Дочерний поток печатает текст...");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Поток был отменен.");
+        }
+        catch (OperationCanceledException)
+        {
+            // Обработка исключения отмены
+            Console.WriteLine("Поток был отменен.");
+        }
+    }
+}
