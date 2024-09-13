a8b5ad996e7a94e1a191b8dac8cae49238280b80
Chanyoll135 <158028703+Chanyoll135@users.noreply.github.com>
2024-09-13 12:30:16 +0300

    Add files via upload

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,41 @@
+﻿class MyThread
+{
+    public void Run() { }
+}
+
+public class Program
+{
+    static Thread parentThread = new Thread(Parent);
+    static Thread childThread = new Thread(Child);
+
+    // Мьютексы для синхронизации потоков
+    static Mutex mutex1 = new Mutex();
+    static Mutex mutex2 = new Mutex();
+
+    static void Main(string[] args)
+    {
+        parentThread.Start();
+    }
+
+    private static void Parent()
+    {
+        while (true)
+        {
+            Console.WriteLine("Родительский поток");
+            mutex1.WaitOne(); // Блокировка мьютекса 1 перед выполнением кода
+            Thread.Sleep(100);
+            childThread.Start();
+            // Разблокировка мьютекса после выполнения кода
+            mutex1.ReleaseMutex();
+        }
+    }
+
+    // Дочерний поток
+    private static void Child()
+    {
+        Console.WriteLine($"Дочерний поток");
+        mutex2.WaitOne();
+        Thread.Sleep(100);
+        mutex1.ReleaseMutex();
+    }
+}
